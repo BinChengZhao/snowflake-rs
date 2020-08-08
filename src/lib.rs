7b25738c79220d4aa7f62f9de4d9e1459f54fa39
BinChengZhao <binchengZhao@outlook.com>
2020-08-08 19:45:28 +0800

    Complete code ，bench and test it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
-//frist for generate
+#![feature(test)]
+use std::{
+    thread::sleep,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
+extern crate test;
+//frist for generate id.
 #[derive(Copy, Clone)]
 pub struct SnowflakeIdGenerator {
     last_time_millis: i64,
@@ -9,14 +13,13 @@ pub struct SnowflakeIdGenerator {
     idx: u16,
 }
 
-//second for batch
-#[derive(Copy, Clone)]
+//second for batch generate id;
+#[derive(Clone)]
 pub struct SnowflakeIdBucket {
     snowflake_id_generator: SnowflakeIdGenerator,
-    bucket: [u64; 4096],
+    bucket: Vec<i64>,
 }
 
-//TODO:写好程序后-依赖方将生成函数内联到程序内部
 impl SnowflakeIdGenerator {
     pub fn new(machine_id: i32, node_id: i32) -> SnowflakeIdGenerator {
         let last_time_millis = SystemTime::now()
@@ -32,29 +35,28 @@ impl SnowflakeIdGenerator {
         }
     }
 
-    pub fn generate(&mut self) -> i64 {
+    //the real_time_generate...
+    pub fn real_time_generate(&mut self) -> i64 {
         self.idx = (self.idx + 1) % 4096;
 
         //if idx == 0 , check last_time_millis is not eq now_time_millis ,that can safe generate
         //huozhe... sleep 1 millis...
 
-        if self.idx == 0 {
-            let now_millis = SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .expect("Time went mackward")
-                .as_millis() as i64;
+        let mut now_millis = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went mackward")
+            .as_millis() as i64;
 
-            if now_millis == self.last_time_millis {
-                //sleep
+        //must use real-time millis generate.
+        if now_millis == self.last_time_millis {
+            //if that millis is exaust, wait....
+            if self.idx == 0 {
+                sleep(Duration::from_millis(1));
+                now_millis += 1;
+                self.last_time_millis = now_millis;
             }
-
-            self.last_time_millis = now_millis;
         }
 
-        // a SnowFlake style
-
-        //https://www.cnblogs.com/jiangxinlingdu/p/8440413.html
-        //what's problem...
         // 64 位，统一左移动 22位，保存后42位 ， xxxx， 最后自增保留12位
         //然后进行或运算
         self.last_time_millis << 22
@@ -62,26 +64,104 @@ impl SnowflakeIdGenerator {
             | ((self.node_id << 12) as i64)
             | (self.idx as i64)
     }
-}
 
+    //basic guarantee time punctuality.
+    //sometimes one millis can't use up 4096 ID, the property of the ID isn't real-time.
+    //But setting time after every 4096 calls.
+    pub fn generate(&mut self) -> i64 {
+        self.idx = (self.idx + 1) % 4096;
 
-impl SnowflakeIdBucket {
-    pub fn get_id(&mut self) -> u64 {
-        todo!();
+        //if idx == 0 , check last_time_millis is not eq now_time_millis ,that can safe generate
+        //huozhe... sleep 1 millis...
+
+        if self.idx == 0 {
+            let mut now_millis = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went mackward")
+                .as_millis() as i64;
+
+            if now_millis == self.last_time_millis {
+                now_millis += 1;
+                sleep(Duration::from_millis(1));
+            }
+
+            self.last_time_millis = now_millis;
+        }
+
+        // 64 位，统一左移动 22位，保存后42位 ， xxxx， 最后自增保留12位
+        //然后进行或运算
+        self.last_time_millis << 22
+            | ((self.machine_id << 17) as i64)
+            | ((self.node_id << 12) as i64)
+            | (self.idx as i64)
     }
 
-    fn generate_ids(&mut self) {
+    //lazy generator.
+    //Just start time record last_time_millis it consume every millis ID.
+    //maybe faster than standing time.
+    pub fn lazy_generate(&mut self) -> i64 {
+        self.idx = (self.idx + 1) % 4096;
 
-        //inline ...
-        //push bucket
-        todo!();
+        if self.idx == 0 {
+            self.last_time_millis += 1;
+        }
+
+        self.last_time_millis << 22
+            | ((self.machine_id << 17) as i64)
+            | ((self.node_id << 12) as i64)
+            | (self.idx as i64)
+    }
+}
+
+impl SnowflakeIdBucket {
+    pub fn new(machine_id: i32, node_id: i32) -> Self {
+        let snowflake_id_generator = SnowflakeIdGenerator::new(machine_id, node_id);
+        let bucket = Vec::new();
+
+        SnowflakeIdBucket {
+            snowflake_id_generator,
+            bucket,
+        }
+    }
+
+    pub fn get_id(&mut self) -> i64 {
+        //247 ns/iter
+        // after self.bucket.push(self.snowflake_id_generator.generate());
+
+        // 7 ns/iter
+        // after self.bucket.push(self.snowflake_id_generator.lazy_generate());
+
+        //45 ns/iter
+        // after self.bucket.push(self.snowflake_id_generator.real_time_generate());
+        if self.bucket.is_empty() {
+            self.generate_ids();
+        }
+        self.bucket.pop().unwrap()
+    }
+
+    pub(crate) fn generate_ids(&mut self) {
+        for _ in 0..4095 {
+
+            // 30,350 -- 50,000 ns/iter
+            //self.bucket.push(self.snowflake_id_generator.lazy_generate());
+            
+            // 1,107,103 -- 1,035,018 ns/iter
+            //self.bucket.push(self.snowflake_id_generator.generate());
+
+            // 408,433 -- 407,926 ns/iter
+            //self.bucket.push(self.snowflake_id_generator.real_time_generate());
+
+            self.bucket
+                .push(self.snowflake_id_generator.lazy_generate());
+        }
     }
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
 
-    use super::SnowflakeIdGenerator;
+    use super::{SnowflakeIdBucket, SnowflakeIdGenerator};
+    use test::Bencher;
 
     #[test]
     fn test_generate() {
@@ -96,5 +176,17 @@ mod test {
             println!("id: {}", id);
             assert!(format!("{}", id).len() >= 18);
         }
+    }
+
+    #[bench]
+    fn bench_generate(b: &mut Bencher) {
+        let mut snowflake_id_bucket = SnowflakeIdBucket::new(1, 1);
+        b.iter(|| snowflake_id_bucket.get_id());
+    }
+
+    #[bench]
+    fn bench_generate_ids(b: &mut Bencher) {
+        let mut snowflake_id_bucket = SnowflakeIdBucket::new(1, 1);
+        b.iter(|| snowflake_id_bucket.generate_ids());
     }
 }
