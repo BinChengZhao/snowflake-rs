95e744664a3f2b6ff39188772721c3b4f5bec553
BinChengZhao <binchengZhao@outlook.com>
2020-08-11 21:01:46 +0800

    Supplement doc.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # snowflake-rs
 The snowflake algorithm rust version.
+
+A crate for quick generating distributed-ids. 
+
+
+API Docs: https://docs.rs/rs-snowflake/0.2.0/snowflake/
+
+## Usage
+
+Add this to your Cargo.toml:
+
+```toml
+[dependencies]
+rs-snowflake = "0.2.0"
+```
+
+
+## Getting Started
+
+```rust
+use snowflake::SnowflakeIdBucket;
+fn main() {
+   let mut id_generator_bucket = SnowflakeIdBucket::new(1, 1);
+   let id = id_generator_bucket.get_id();
+}
+```
+     
+
+## License
+
+Licensed under
+
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+
+### Contribution
+
+Let me see, what do I write.
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,50 @@
+//! The snowflake algorithm rust version.
+//!
+
 #![feature(test)]
 use std::{
     thread::sleep,
     time::{Duration, SystemTime, UNIX_EPOCH},
 };
 extern crate test;
-//frist for generate id.
+
+/// The `SnowflakeIdGenerator` type is snowflake algorithm wrapper.
 #[derive(Copy, Clone)]
 pub struct SnowflakeIdGenerator {
+    /// last_time_millis, last time generate id is used times millis.
     last_time_millis: i64,
+
+    /// machine_id, is use to supplement id machine or sectionalization attribute.
     machine_id: i32,
+
+    /// node_id, is use to supplement id machine-node attribute.
     node_id: i32,
+
+    /// auto-increment record.
     idx: u16,
 }
 
-//second for batch generate id;
+/// The `SnowflakeIdBucket` type is snowflake-id-bucket it easy to get id also have a id buffer.
 #[derive(Clone)]
 pub struct SnowflakeIdBucket {
+    /// Hidden the `SnowflakeIdGenerator` in bucket .
     snowflake_id_generator: SnowflakeIdGenerator,
+
+    /// The bucket buffer;
     bucket: Vec<i64>,
 }
 
 impl SnowflakeIdGenerator {
+    /// Constructs a new `SnowflakeIdGenerator`.
+    /// Please make sure that machine_id and node_id is small than 32(2^5);
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use snowflake::SnowflakeIdGenerator;
+    ///
+    /// let id_generator = SnowflakeIdGenerator::new(1, 1);
+    /// ```
     pub fn new(machine_id: i32, node_id: i32) -> SnowflakeIdGenerator {
         let last_time_millis = SystemTime::now()
             .duration_since(UNIX_EPOCH)
@@ -35,7 +59,16 @@ impl SnowflakeIdGenerator {
         }
     }
 
-    //the real_time_generate...
+    /// The real_time_generate keep id generate time is eq call method time...
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use snowflake::SnowflakeIdGenerator;
+    ///
+    /// let mut id_generator = SnowflakeIdGenerator::new(1, 1);
+    /// id_generator.real_time_generate();
+    /// ```
     pub fn real_time_generate(&mut self) -> i64 {
         self.idx = (self.idx + 1) % 4096;
 
@@ -59,14 +92,24 @@ impl SnowflakeIdGenerator {
             self.last_time_millis = now_millis;
         }
 
-        // 64 位，统一左移动 22位，保存后42位 ， xxxx， 最后自增保留12位
-        //然后进行或运算
+        //last_time_millis is 64 bits，left shift 22 bit，store 42 bits ， machine_id left shift 17 bits，
+        // node_id left shift 12 bits ,idx complementing bits.
         self.last_time_millis << 22
             | ((self.machine_id << 17) as i64)
             | ((self.node_id << 12) as i64)
             | (self.idx as i64)
     }
 
+    /// The basic guarantee time punctuality...
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use snowflake::SnowflakeIdGenerator;
+    ///
+    /// let mut id_generator = SnowflakeIdGenerator::new(1, 1);
+    /// id_generator.generate();
+    /// ```
     //basic guarantee time punctuality.
     //sometimes one millis can't use up 4096 ID, the property of the ID isn't real-time.
     //But setting time after every 4096 calls.
@@ -98,7 +141,17 @@ impl SnowflakeIdGenerator {
             | (self.idx as i64)
     }
 
-    //lazy generator.
+    /// Thelazy generate...
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use snowflake::SnowflakeIdGenerator;
+    ///
+    /// let mut id_generator = SnowflakeIdGenerator::new(1, 1);
+    /// id_generator.lazy_generate();
+    /// ```
+    //lazy generate.
     //Just start time record last_time_millis it consume every millis ID.
     //maybe faster than standing time.
     pub fn lazy_generate(&mut self) -> i64 {
@@ -116,6 +169,16 @@ impl SnowflakeIdGenerator {
 }
 
 impl SnowflakeIdBucket {
+    /// Constructs a new `SnowflakeIdBucket`.
+    /// Please make sure that machine_id and node_id is small than 32(2^5);
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use snowflake::SnowflakeIdBucket;
+    ///
+    /// let id_generator_bucket = SnowflakeIdBucket::new(1, 1);
+    /// ```
     pub fn new(machine_id: i32, node_id: i32) -> Self {
         let snowflake_id_generator = SnowflakeIdGenerator::new(machine_id, node_id);
         let bucket = Vec::new();
@@ -126,6 +189,17 @@ impl SnowflakeIdBucket {
         }
     }
 
+    /// Generate id.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use snowflake::SnowflakeIdBucket;
+    ///
+    /// let mut id_generator_bucket = SnowflakeIdBucket::new(1, 1);
+    /// let id = id_generator_bucket.get_id();
+    ///
+    /// ```
     pub fn get_id(&mut self) -> i64 {
         //247 ns/iter
         // after self.bucket.push(self.snowflake_id_generator.generate());
@@ -160,7 +234,7 @@ impl SnowflakeIdBucket {
 #[cfg(test)]
 mod tests {
 
-    use super::{SnowflakeIdBucket, SnowflakeIdGenerator};
+    use crate::{SnowflakeIdBucket, SnowflakeIdGenerator};
     use test::Bencher;
 
     #[test]
@@ -183,7 +257,6 @@ mod tests {
         }
     }
 
-
     #[test]
     fn test_real_time_generate() {
         let mut id_generator = SnowflakeIdGenerator::new(1, 1);
@@ -204,7 +277,6 @@ mod tests {
         }
     }
 
-
     #[test]
     fn test_lazy_generate() {
         let mut id_generator = SnowflakeIdGenerator::new(1, 1);
@@ -220,7 +292,7 @@ mod tests {
 
             assert_eq!(10000, ids.len());
             println!("{}", ids[9999]);
-            
+
             ids.clear();
         }
     }
